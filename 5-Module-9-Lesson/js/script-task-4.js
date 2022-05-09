09128a8ce50b6fdc06eda69f65a60871c9a8e4f0
predator-1 <golubevmusic@gmail.com>
2022-05-09 19:08:33 +0300

    lesson 9

---
+++ 5-Module-9-Lesson/js/script-task-4.js
@@ -0,0 +1,5 @@
+/**
+ * Створити об'єкт person1 з полем name та getName яка поверне поле name
+ * Створити об'єкт person2 з полем name та присвоїти person2.getName = person1.getName
+ * Вивести результат обох getName в консоль
+ */
