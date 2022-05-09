5f9d021d1def4f2c320fe4b9b77dc25b8811544f
predator-1 <golubevmusic@gmail.com>
2022-05-09 19:08:44 +0300

    lesson 9

---
+++ 5-Module-9-Lesson/js/script-task-2.js
@@ -0,0 +1,34 @@
+// Виконайте рефакторинг методів об'єкта phonebook, щоб код запрацював.
+
+const phonebook = {
+  contacts: [],
+  add(contact) {
+    const newContact = {
+      list: 'default',
+      ...contact,
+      id: generateId(),
+      createdAt: getDate(),
+    };
+    contacts.push(newContact);
+  },
+  generateId() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  },
+  getDate() {
+    return Date.now();
+  },
+};
+
+console.log(
+  phonebook.add({
+    name: 'Mango',
+    email: 'mango@mail.com',
+    list: 'friends',
+  }),
+);
+console.log(
+  phonebook.add({
+    name: 'Poly',
+    email: 'poly@hotmail.com',
+  }),
+);
---
+++ 5-Module-9-Lesson/js/script-task-5.js
@@ -0,0 +1,14 @@
+// fix errors
+
+const myself = {
+  myName: 'Anna',
+  myNickNames: ['Kitty', 'Dorsy'],
+  printMyNickNames: function () {
+    'use strict';
+    this.myNickNames.forEach(nickName =>
+      console.log(this.myName + ' is also known as ' + nickName),
+    );
+  },
+};
+
+myself.printMyNickNames();
