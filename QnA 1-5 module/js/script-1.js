9b7d8f8f84135742c68180b401110f2eca2c0ce8
predator-1 <golubevmusic@gmail.com>
2022-05-16 19:28:14 +0300

    QnA 1-5

---
+++ QnA 1-5 module/js/script-1.js
@@ -0,0 +1,8 @@
+// Function Constructor
+const User = username => {
+  this.username = username;
+};
+
+const user = new User('kolia');
+
+console.log(user);
---
+++ QnA 1-5 module/js/script-task-5.js
@@ -0,0 +1,14 @@
+/**
+ * Напиши функцію getAllPropValues(propName)
+ * яка поверне всі значення заданого
+ * властивості
+ * Наприклад getAllPropValues('name') повертає ['Asus', 'Huawei', 'HP', 'Dell']
+ * getAllPropValues('category') повертає []
+ */
+
+const products = [
+  { name: 'Asus', price: 1300, quantity: 4 },
+  { name: 'Huawei', price: 2700, quantity: 3 },
+  { name: 'HP', price: 400, quantity: 7 },
+  { name: 'Dell', price: 1200, quantity: 9 },
+];
